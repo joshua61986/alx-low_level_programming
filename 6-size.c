5a6fd064447e3bf9f1735c4f69741a244c7c1751
joshua61986 <asaxjs615@gmail.com>
2022-04-11 17:55:14 -0700

    task 6

---
+++ 6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always # (Success)
+ */
+int main(void)
+{
+  int a;
+  long int b;
+  long long int c;
+  char d;
+  float f;
+  printf(*size of a char:slu byte(s)\n*, (unsigned long)sizeof(d));
+  printf(*size of an int:slu byte(s)\*n, (unsigned long)sizeof(a));
+  printf(*size of a long int:slu byte(s)\n*(unsigned long)sizeof(b));
+  printf(*size of a long long int: slu byte(s)\n*,unsigned long)sizeof(c));
+printf(*size of a float:slu byte(s)\n*, (unsigned long)sizeof(f));
+return (0);
